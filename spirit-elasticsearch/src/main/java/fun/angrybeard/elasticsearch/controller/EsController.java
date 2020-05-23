7281a48969c2424096c189b758c85fd0b4b7be46
angry_beard <better_river@163.com>
2020-05-23 16:43:52 +0800

    :recycle: Refactoring code.

--- spirit-elasticsearch/src/main/java/fun/angrybeard/elasticsearch/controller/EsController.java
+++ spirit-elasticsearch/src/main/java/fun/angrybeard/elasticsearch/controller/EsController.java
@@ -3,9 +3,12 @@ package fun.angrybeard.elasticsearch.controller;
 import fun.angrybeard.elasticsearch.dao.IUserDao;
 import fun.angrybeard.elasticsearch.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 /**
  * Created by angry_beary on 2019/11/10.
@@ -20,5 +23,20 @@ public class EsController {
     @PostMapping("add-user")
     public User addUser(@RequestBody User user) {
         return userDao.save(user);
+    }
+
+    @GetMapping("query-all-user")
+    public Iterable<User> queryAll() {
+        return userDao.findAll();
+    }
+
+    @GetMapping("query-user")
+    public Optional<User> query(String id) {
+        return userDao.findById(id);
+    }
+
+    @GetMapping("query-word")
+    public String query() {
+        return "Hello World";
     }
 }
